ccb065b465aded0440edda3fe934b4d73a10e5b6
Tom Camp <tom.camp@civicactions.com>
2026-03-08 18:21:44 -0400

    fix:adding initial migration commit.

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,12 +1,8 @@
 from sqlalchemy import Engine
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import create_engine
 
 from app.utils.config import settings
 
 
 def make_engine() -> Engine:
     return create_engine(settings.database_url, echo=True)
-
-
-def create_db_and_tables(engine: Engine) -> None:
-    SQLModel.metadata.create_all(engine)
